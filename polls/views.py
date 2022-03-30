6cdac2f2c9cd671e53b8ef1b08f2344c13f4eab1
BulliedDog <albertorovai1@gmail.com>
2022-03-30 18:36:14 +0200

    commit

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render
 from polls.models import Question
 # Create your views here.
 def index(request):
     latest_questions_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_questions_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_questions_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
@@ -16,5 +17,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-# Gotta add some better views here :)
